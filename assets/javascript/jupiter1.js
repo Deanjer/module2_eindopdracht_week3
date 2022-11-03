2e7b9f199b8a597f4645b5afea9f94b2a0ea6569
Deanjer <220509@student.glu.nl>
2022-11-03 13:26:10 +0100

    module2_eindopdracht

---
+++ assets/javascript/jupiter1.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($){
+
+    $('.jupiter-button').on({
+         'click': function(){
+             $('#change-image').attr('src','../images/planet-jupiter-vector-11088916.jpeg');
+         }
+     });
+     
+    $('.earth-button').on({
+         'click': function(){
+             $('#change-image').attr('src','../images/planet-earth-vector-11088906.jpeg');
+         }
+     });
+     
+    $('.saturn-button').on({
+         'click': function(){
+             $('#change-image').attr('src','../images/planet-saturn-vector-11088920.jpeg');
+         }
+     });
+     
+    $('.venus-button').on({
+         'click': function(){
+             $('#change-image').attr('src','../images/planet-venus-vector-10652806.jpeg');
+         }
+     });
+    });
